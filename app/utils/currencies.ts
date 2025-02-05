df7bd4019bf6c4c7b406afa4b2a917e2523198f2
Evan Huang <evan.huang000@proton.me>
2025-02-05 16:00:38 +0800

    Create currencies.ts

---
+++ app/utils/currencies.ts
@@ -0,0 +1,15 @@
+export const currencySymbols: { [key: string]: string } = {
+  USD: "$",
+  EUR: "€",
+  GBP: "£",
+  JPY: "¥",
+  CNY: "¥",
+}
+
+export const currencyOptions = [
+  { value: "USD", symbol: "$", name: "US Dollar" },
+  { value: "EUR", symbol: "€", name: "Euro" },
+  { value: "GBP", symbol: "£", name: "British Pound" },
+  { value: "JPY", symbol: "¥", name: "Japanese Yen" },
+  { value: "CNY", symbol: "¥", name: "Chinese Yuan" },
+]
